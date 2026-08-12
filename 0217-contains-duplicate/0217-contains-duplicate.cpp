51959157dae1c1b9e62f20dae2625be437e9c83c
ZAID IQBAL <117713986+zaidiqbal17@users.noreply.github.com>
2026-08-12 18:16:30 +0530

    Time: 68 ms (54.49%), Space: 111.2 MB (48.18%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int num : nums){
+            mp[num]++;
+            if(mp[num]>1){
+                return true;
+            }
+        }
+       
+        return false;
+    }
+};
