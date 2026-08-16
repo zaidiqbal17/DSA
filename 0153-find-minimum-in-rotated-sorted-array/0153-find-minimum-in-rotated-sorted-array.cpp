8c8f063d7414acd1779ddd90b08f636d68c09ed2
ZAID IQBAL <117713986+zaidiqbal17@users.noreply.github.com>
2026-08-16 19:59:01 +0530

    Time: 0 ms (100%), Space: 14.2 MB (50.79%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,10 +7,10 @@ public:
 
         while(low<=high){
             int mid = (low+high)>>1;
-            if(nums[low]<=nums[high]){
-                ans=min(ans,nums[low]);
-                break;
-            }
+            // if(nums[low]<=nums[high]){
+            //     ans=min(ans,nums[low]);
+            //     break;
+            // }
 
             if(nums[low]<=nums[mid]){
                 ans = min(ans,nums[low]);
