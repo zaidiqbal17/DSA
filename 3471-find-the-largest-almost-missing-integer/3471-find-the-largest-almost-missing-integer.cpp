6ecfff5e1f303168677a2aae02c5c3db3bd70b8b
ZAID IQBAL <117713986+zaidiqbal17@users.noreply.github.com>
2026-08-18 11:41:20 +0530

    Time: 0 ms (100%), Space: 29.2 MB (73.68%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int, int> freq;
-        for (int num : nums) {
-            freq[num]++;
+
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
         }
 
         if (k == 1) {
+            int freq[51] = {0};
+            for (int x : nums) {
+                freq[x]++;
+            }
             int ans = -1;
-            for (auto& [val, count] : freq) {
-                if (count == 1) {
-                    ans = max(ans, val);
+            for (int val = 50; val >= 0; val--) {
+                if (freq[val] == 1) {
+                    return val;
                 }
             }
-            return ans;
-        }
-
-        if (k == n) {
-            int ans = -1;
-            for (int num : nums) {
-                ans = max(ans, num);
-            }
-            return ans;
+            return -1;
         }
 
         int ans = -1;
-        if (freq[nums[0]] == 1) {
+        
+        int countFirst = count(nums.begin(), nums.end(), nums[0]);
+        if (countFirst == 1) {
             ans = max(ans, nums[0]);
         }
-        if (freq[nums[n - 1]] == 1) {
+
+        int countLast = count(nums.begin(), nums.end(), nums[n - 1]);
+        if (countLast == 1) {
             ans = max(ans, nums[n - 1]);
         }
 
