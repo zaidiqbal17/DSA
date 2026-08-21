e3aebe5a723c89ede15e890688cf1ede760de504
ZAID IQBAL <117713986+zaidiqbal17@users.noreply.github.com>
2026-08-21 19:33:14 +0530

    Time: 7 ms (86.78%), Space: 14.1 MB (14.8%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -20,11 +20,11 @@ public:
             if (currentSum < target) {
                 left++;
             } 
-            // else if (currentSum > target) {
-            //     right--;
-            // } 
+            else if (currentSum > target) {
+                right--;
+            } 
             else {
-                right--;  
+                break;  
             }
         }
     }
